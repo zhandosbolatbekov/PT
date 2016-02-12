51856610ec4ad8989c1d8390c1ac989631062b54
zhandosbolatbekov <zhabo.fm@gmail.com>
2016-02-12 13:38:14 +0600

    remade

---
+++ week 1/Complex/Complex/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    class Complex
+    {
+        public int a, b;
+        public Complex() { }
+        public Complex(int x, int y)
+        {
+            a = x;
+            b = y;
+        }
+        public static int gcd(int x, int y)
+        {
+            while (x > 0 && y > 0)
+                if (x > y)
+                    x %= y;
+                else
+                    y %= x;
+            return x + y;
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            int q = c1.b * c2.b;
+            int p = c1.a * c2.b + c1.b * c2.a;
+            int g = gcd(p, q);
+            return new Complex(p / g, q / g);
+        }
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex A = new Complex(4, 5);
+            Complex B = new Complex(2, 3);
+            Complex C = A + B;
+            Console.WriteLine(C.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week 1/Example3/Example3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"c:\testfolder");
+            int cnt = dfs(d);
+            Console.WriteLine(cnt);
+            Console.ReadKey();
+        }
+        static int dfs(DirectoryInfo d)
+        {
+            Stack<DirectoryInfo> st = new Stack<DirectoryInfo>();
+            st.Push(d);
+            int cnt_files = 0;
+            while (st.Count > 0)
+            {
+                DirectoryInfo v = st.Peek();
+                Console.WriteLine(v.FullName);
+                st.Pop();
+                FileInfo[] files = v.GetFiles();
+                cnt_files += files.Length;
+                DirectoryInfo[] dirs = v.GetDirectories();
+                foreach(DirectoryInfo to in dirs) {
+                    st.Push(to);
+                }
+            }
+
+            return cnt_files;
+        }
+    }
+}
